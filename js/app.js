4cca76d39ef15abdbb85d2cf12e804dd99780509
Gordon Watt <gordon@morningglass.co.uk>
2026-03-26 20:18:47 +0000

    Revert "Convert About from modal to card overlay"
    This reverts commit 043ac88fcfa7bac8ae3a3123cb3a5114eedab85e.

--- js/app.js
+++ js/app.js
@@ -320,9 +320,9 @@ window.toggleSidebar = function() {
   }
 };
 
-/* ---- About card ---- */
-window.openAbout  = () => document.getElementById('about-card-overlay').classList.add('open');
-window.closeAbout = () => document.getElementById('about-card-overlay').classList.remove('open');
+/* ---- About modal ---- */
+window.openAbout  = () => document.getElementById('about-modal').classList.add('open');
+window.closeAbout = () => document.getElementById('about-modal').classList.remove('open');
 
 /* ---- Apply filters ---- */
 function applyFilters() {
